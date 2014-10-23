b0bd0d09baf65e75f53d8c41d6d8097805207f4b
iLoop2 <iLoop2@users.noreply.github.com>
2014-10-23 07:57:13 +0200

    Update to latest lib

--- dune-istl.rb
+++ dune-istl.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class DuneIstl < Formula
   homepage 'http://www.dune-project.org/'
-  url 'http://www.dune-project.org/download/2.2.1/dune-istl-2.2.1.tar.gz'
-  sha1 'e213d2daa5c5f330d397e7951544170151042f8d'
+  url 'http://www.dune-project.org/download/2.3.1/dune-istl-2.3.1.tar.gz'
+  sha1 '68f2f351be5586b1123e9900588a3bfd35dd1591'
 
   depends_on 'autoconf'  => :build
   depends_on 'automake'  => :build
@@ -20,43 +20,43 @@ class DuneIstl < Formula
 	
   def install
     # parse version number
-    major = version.to_s.split('.')[0]
-    minor = version.to_s.split('.')[1]
+ #   major = version.to_s.split('.')[0]
+ #   minor = version.to_s.split('.')[1]
 
     # parse name
-    suite = name.to_s.split('-')[0]
-    modul = name.to_s.split('-')[1]
+  #  suite = name.to_s.split('-')[0]
+  #  modul = name.to_s.split('-')[1]
 
 	# avoid getting into a conflict with version in the Cellar
-	ENV.append 'DUNE_CONTROL_PATH', "#{HOMEBREW_PREFIX}"
+#	ENV.append 'DUNE_CONTROL_PATH', "#{HOMEBREW_PREFIX}"
 
     # generate configuration files (since we modified build-system)
-    system 'touch', 'stamp-vc'
-	system "#{HOMEBREW_PREFIX}/bin/dunecontrol", '--current', 'autogen'
+    #system 'touch', 'stamp-vc'
+#	system "#{HOMEBREW_PREFIX}/bin/dunecontrol", '--current', 'autogen'
 
     # there is a test which includes stdlib.h, which apparently needs
     # this define if we are using clang
-    if build.with? 'mpi' and ENV.compiler == :clang
-	  ENV.append 'CXXFLAGS', ' -D_WCHAR_T'
-    end
+   # if build.with? 'mpi' and ENV.compiler == :clang
+#	  ENV.append 'CXXFLAGS', ' -D_WCHAR_T'
+ #   end
 
-    args = %W[ --enable-fieldvector-size-is-method
-               --enable-shared
-               --prefix=#{prefix}
-             ]
-    args << "--enable-parallel" if build.with? 'mpi'
+  #  args = %W[ --enable-fieldvector-size-is-method
+  #             --enable-shared
+  #             --prefix=#{prefix}
+  #           ]
+  #  args << "--enable-parallel" if build.with? 'mpi'
 
 	# use C++11 runtime library
-    if MacOS.version >= :lion and build.with? 'c++11'
-	  if ENV.compiler == :clang
-		ENV.append 'CXX', ' -stdlib=libc++'
-	  end
-	else
-	  args << "--disable-gxx0xcheck"
-    end
+   # if MacOS.version >= :lion and build.with? 'c++11'
+#	  if ENV.compiler == :clang
+#		ENV.append 'CXX', ' -stdlib=libc++'
+#	  end
+#	else
+#	  args << "--disable-gxx0xcheck"
+#    end
   
-    system "#{HOMEBREW_PREFIX}/bin/dunecontrol", "--configure-opts=#{args.join(' ')}", '--current', 'configure'
-
+    #system "#{HOMEBREW_PREFIX}/bin/dunecontrol", "--configure-opts=#{args.join(' ')}", '--current', 'configure'
+    system "./configure", "--prefix=#{prefix}"
     # compile and copy target files to their final location
     system 'make', 'install'
   end
