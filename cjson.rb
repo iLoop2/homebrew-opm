2d4f4226ef4cbdd938476d0b67e0fafd716952c5
Fredrik Gundersen <fredrik.gundersen@mac.com>
2014-10-23 18:22:55 +0200

    Test 3

--- cjson.rb
+++ cjson.rb
@@ -1,18 +1,23 @@
  # This file is licensed under the GNU General Public License v3.0
 require 'formula'
 
-class DuneGrid < Formula
-  homepage 'http://www.dune-project.org/'
-  url 'http://www.dune-project.org/download/2.3.1/dune-grid-2.3.1.tar.gz'
-  sha1 'e00d2386ddbfb73f57e67e9d3c107e59d4232dee'
+class cjson < Formula
+  homepage 'http://sourceforge.net/projects/cjson'
+  url 'http://sourceforge.net/projects/cjson/files/cJSONFiles.zip/download'
+  sha1 '3a016bc2b75a2eafa5f9ad192b0154a9fa2b2abe'
 
   depends_on 'autoconf'  => :build
   depends_on 'automake'  => :build
   depends_on 'libtool'   => :build
   depends_on 'pkgconfig' => :build
-  
-  depends_on 'dune-geometry'
-  
+
+  # use Apple's Accelerate framework instead
+
+  depends_on 'gmp'
+  depends_on 'metis4'
+  depends_on 'superlu'
+  option 'mpi', 'Enable MPI support'
+
 
   # if we are built with c++11, make sure we link to a boost that is
   depends_on 'boost' => ('--with-c++11' if build.with? 'c++11')
@@ -29,3 +34,5 @@ class DuneGrid < Formula
 
   end
 end
+
+
